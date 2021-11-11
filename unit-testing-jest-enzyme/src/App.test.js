96b6f8b915ba379518ea0ef3efbe3fce130eb07a
Shaik Rasool <shaik.web27@gmail.com>
2021-11-11 10:26:54 +0530

    code update

--- unit-testing-jest-enzyme/src/App.test.js
+++ unit-testing-jest-enzyme/src/App.test.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import {applyMiddleware, createStore} from 'redux';
+
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import {Provider} from "react-redux";
+import React from 'react';
+import {configure} from 'enzyme';
 import { mount } from 'enzyme';
+import reducers from './reducers';
 
 // We need to wrap CommentBox with <Provider> tag in first beforeEach(() => {}) below;
 // otherwise we receive this error message:
 // Invariant Violation: Could not find “store” in either the context or props of “Connect(CommentBox)”
 // https://stackoverflow.com/questions/36211739/invariant-violation-could-not-find-store-in-either-the-context-or-props-of-c
 // Also see comment_list.test.js
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware} from 'redux';
-import reducers from './reducers';
 
 configure({adapter: new Adapter()});
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-// Use 'describe' to group together similar tests
+// Use 'describe' to group  similar tests
 describe('App', () => {
 
     let component;
